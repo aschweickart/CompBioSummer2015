ab1cf531497cd67ff3c65d9d3b5d50557ef3729b
Juliet <jforman@hmc.edu>
2015-06-10 10:29:49 -0700

    make small DTL example from ThreeTips

---
+++ TestDTL.py
@@ -0,0 +1,6 @@
+DTL = {}
+DTL[(p6, h6)] = [['Speciation', (p8, h8), (p1, h1), 1] 0]
+DTL[(p8, h8)] = [['Speciation', (p3, h3), (p2, h2), 1] 0]
+DTL[(p1, h1)] = [['C', None, None, 1] 0]
+DTL[(p3, h3)] = [['C', None, None, 1] 0]
+DTL[(p2, h2)] = [['C', None, None, 1] 0]
