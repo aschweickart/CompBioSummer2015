84c086a209c23f16e82e399d5aa32cb47307aa60
Carter <carter440@gmail.com>
2015-06-10 01:18:37 -0700

    cleaning

--- DP.py
+++ DP.py
@@ -227,13 +227,11 @@ def DP(hostTree, parasiteTree, phi, D, T, L):
     # print BestSwitchLocations
     for key in Dictionary.keys():
         Dictionary[key].append(Minimums[key])
-<<<<<<< HEAD
-    return Minimums, Dictionary
-=======
+
     treeMin = findBest(parasiteTree, Minimums)
     
     return DTL
->>>>>>> origin/Annalise's-Branch
+
 
 
 def findBest(Parasite, MinimumDict):
