f8adc149ccdaf7b9d7d49397f64727138d7b4dc8
Juliet <jforman@hmc.edu>
2015-06-11 12:19:00 -0700

    more comments, simplify code

--- Greedy.py
+++ Greedy.py
@@ -4,61 +4,65 @@
 
 #Our goal is to find the k best reconciliations
 
-#We are given a DTL reconciliation graph in the form of dictionaries
-
-#Dict1: the values of the DTl graphs will be modified to hold the BSFH scores
-
-#Keeping track of bookkeeping
-
-#key: mapping node
-#value: List of (0) max (1) where the max came from
-
-
-def findRoot(parasiteTree):
+def findRoot(ParasiteTree):
     """This function takes in a parasiteTree and returns a string with the name of
     the root vertex of the tree"""
-    root = parasiteTree['pTop'][1]
+    root = ParasiteTree['pTop'][1]
     return root
 
 
-def postorderDTLwrapper(DTL, ParasiteRoot):
-    """This wrapper function uses postorderDTL and starts with a level of 0"""
+def orderDTLwrapper(DTL, ParasiteTree):
+    """This wrapper function uses orderDTL and starts with a level of 0. Returns keysL."""
     postorderDTL(DTL, ParasiteTree, 0)
 
-def postorderDTL(DTL, ParasiteRoot, level):
+def orderDTL(DTL, ParasiteTree, level):
     """This function takes in a DTL dictionary, a ParasiteRoot, and a level that represents the depth of 
-    the of a vertex pair. This postorder function returns a list, keysL, that includes tuples with the first 
-    elements being a mapping node of the form (p, h), and the second element the depth of that node within 
-    the graph. This function loops through the DTL graph and recruses on the two children of the DTL 
-    mapping node."""
+    the of a vertex pair. It returns a list, keysL, that includes tuples with the first 
+    elements being a mapping node of the form (p, h), and the second element being the depth of that node within 
+    the graph. This function loops through the DTL graph and recruses on the two children of each DTL 
+    mapping node, adding the results to keysL"""
 
+    ParasiteRoot = findRoot(ParasiteTree)
     keysL = []
-    print ParasiteRoot
     for key in DTL:
         if key[0] == ParasiteRoot:
-            for i in range(len(DTL[key]) - 1):
+            for i in range(len(DTL[key]) - 1):          #loop through each event associated with key in DTL
                 event = DTL[key][i]
                 child1 = event[1]
                 child2 = event[2]
-                if child1[0] == None and child2[0] == None:
+                if child1[0] == None and child2[0] == None:    #base case: mapping node (key) is a tip
                     keysL = keysL + [(key, level)]
-                elif child2[0] == None:
-                    keysL = keysL + [(key, level)] + [postorderDTL(DTL, child1[0], level + 1)]
-                else:
-                    keysL = keysL + [(key, level)] + [postorderDTL(DTL, child2[0], level + 1)]
-
+                elif child2[0] == None:                        #loss case: there is only one child (child1)
+                    keysL = keysL + [(key, level)] + [orderDTL(DTL, child1[0], level + 1)]
+                else:                                          #loss case: there is only one child (child2)
+                    keysL = keysL + [(key, level)] + [orderDTL(DTL, child2[0], level + 1)]
     return keysL
 
-def postorderDTLsort(keysL):
-    """This takes in the output list of postorderDTL and returns a sorted list, orderedKeysL, that is ordered
-    by level from largest to smallest."""
+
+def postorderDTLsort(DTL, ParasiteTree):
+    """This takes in a DTL dictionary and parasite tree and returns a sorted list, orderedKeysL, that is ordered
+    by level from largest to smallest, where level 0 is the root and the highest level has tips."""
+    keysL = orderDTLwrapper(DTL, ParasiteTree)
     orderedKeysL = []
     levelCounter = 0
     while len(orderedKeysL) < len(keysL):
         for mapping in keysL:
             if mapping[-1] == levelCounter:
                 orderedKeysL = [mapping] + orderedKeysL
+                levelCounter += 1
+    return orderedKeysL
 
+def preorderDTLsort(DTL, ParasiteTree):
+    """This takes in a DTL dictionary and a parasite tree and returns a sorted list, orderedKeysL, that is ordered
+    by level from smalles to largest, where level 0 is the root and the highest level has tips."""
+    keysL = orderDTLwrapper(DTL, ParasiteTree)
+    orderedKeysL = []
+    levelCounter = 0
+    while len(orderedKeysL) < len(keysL):
+        for mapping in keysL:
+            if mapping[-1] == levelCounter:
+                orderedKeysL = [mapping] + orderedKeysL
+                levelCounter += 1
     return orderedKeysL
 
 
@@ -66,37 +70,38 @@ def postorderDTLsort(keysL):
 
 def bookkeeping(DTL, ParasiteRoot):
     """This function inputs the DTL graph and ParasiteTree, and then records what the max is at each mapping node and 
-    where the max came from. It outputs two dictionaries BSFHMap, and BSFHEvent, by looping through the keys in orderedKeysL and 
-    finding the max score at each mapping node and event node"""
+    where the max came from. It outputs two dictionaries BSFHMap and BSFHEvent, by looping through the keys in orderedKeysL 
+    and finding the max score at each mapping node and event node"""
 
     """We are creating two dictionaries. BSFHMap has keys of the form (p, h) which are the mapping nodes, and values
-    which are lists of length """
+    which are lists where the first element is a list of events with the max score, and the last element is the max score.
+    BSFHEvent is a dictionary with events as keys, and values which are one number which is the max score."""
 
     #BSFHMap = {(mapping node): [['event', (p, h), (p, h), score], maxScore]}
     #BSFHEvent = {(event node): max}
 
-    #PROBTIP = 1 this will change depending on how we normalize 
+    TIPSCORE = 1 #this will change depending on how we define the score for tips
 
     BSFHMap = {}
     BSFHEvent = {}
 
-    BSFHMap[None] = 0
+    BSFHMap[(None, None)] = 0           #BSFH value for empty children is 0
 
     orderedKeys = postorderDTLsort(postorderDTLwrapper(DTL, ParasiteRoot))
 
     for key in orderedKeys:
         if DTL[key[0]][0][0] == 'C':                   #check if the key is a tip
-           BSFHMap[key] = [DTL[key][0], 1]    #set BSFH of tip to some global variable (1 should be PROBTIP)
+           BSFHMap[key] = [DTL[key][0], TIPSCORE]    #set BSFH of tip to some global variable
         else:                                       #if key isn't a tip:
             maxScore = 0                             #initialize counter
             maxEvent = []                           #initialize variable to keep track of where max came from
             for i in range(length(DTL[key]) - 1):   #iterate through the events associated with the key node
-                event = DTL[key][i]                 #set variable name that makes sense
+                event = DTL[key][i]
                 BSFHEvent[event] = BSFHMap[event[1]] + BSFHMap[event[2]]
-                if BSFHEvent[event][-1] > maxScore:  #check if current event has a higher prob than current max
-                    maxScore = BSFHEvent[event][-1]  #if so, set new max prob
+                if BSFHEvent[event][-1] > maxScore:  #check if current event has a higher score than current max
+                    maxScore = BSFHEvent[event][-1]  #if so, set new max score
                     maxEvent = event                #record where new max came from
-                elif BSFHEvent[event][-1] == maxScore:
+                elif BSFHEvent[event][-1] == maxScore: # if event score ties with another event, add both to the dictionary
                     maxEvent.append(event)
             BSFHMap[key] = [maxEvent, maxScore]      #set BSFH value of key
 
