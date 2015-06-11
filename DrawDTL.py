535d45396adf264b9085151c875082914dc10488
Carter <carter440@gmail.com>
2015-06-10 22:28:19 -0700

    Helper function connect added

--- DrawDTL.py
+++ DrawDTL.py
@@ -1,4 +1,25 @@
 
 import turtle
 
+NodeLocations = {}
 
+
+
+
+#tells a turtle to draw a line from start to end and returns to its position before the function call
+def connect(Turtle,Start,End):
+	loc = Turtle.pos()
+	Turtle.penup()
+	Turtle.setpos(Start,None)
+	Turtle.pendown()
+	Turtle.goto(End,None)
+	Turtle.stamp()
+	Turtle.penup()
+	Turtle.goto(loc,None)
+	Turtle.pendown()
+
+
+
+
+def DrawDTL(treeMin,DTL):
+	Bob = turtle.Turtle()
