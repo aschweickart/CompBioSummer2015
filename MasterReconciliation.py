490e9cf43b1f85cdb199010f29a6d94b496f9e26
Carter <carter440@gmail.com>
2015-06-17 00:45:39 -0700

    fdsaff

---
+++ MasterReconciliation.py
@@ -0,0 +1,13 @@
+import DP
+import Greedy
+import newickToVis
+import ReconConversion
+
+def Reconcile(fileName, D, T, L, k):
+	newickToVis.convert(fileName)
+	host, paras, phi = DP.newickFormatReader.getInput(fileName)
+	DTL = DP.DP(host, paras, phi, D, T, L)
+	rec = Greedy.Greedy(DTL, paras, k)
+	newickToVis(fileName)
+	ReconConversion.convert(rec[0], fileName[:-7])
+	return DTL, rec
