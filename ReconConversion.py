a44b394e6a83a51a536626976146edceafc8c9ef
Juliet <jforman@hmc.edu>
2015-06-17 16:54:45 -0700

    make convert prettier with a dictionary

--- ReconConversion.py
+++ ReconConversion.py
@@ -1,20 +1,14 @@
+
 # PROF WU VISUALIZATION RECONCILIATION FORMAT CONVERSION
 def convert(reconciliation, outputFile):
-	"""Takes as input a dictionary of a reconciliation between host and parasite trees and the name of a file
-	where it will put the output. The function outputs the same tree converted to brecon (gene node, species node, 
-	event) format."""
-
+	"""Takes as input a dictionary of a reconciliation between host and parasite trees and a string containing the 
+	name of a file where it will put the output. The function outputs the same tree converted to brecon format."""
+	
+	D = {'T': 'trans', 'S': 'spec', 'L': 'loss', 'C': 'gene'}
 	f = open(outputFile + ".mowgli.brecon", 'w')
 	event = ""
 	for key in reconciliation:
-		if reconciliation[key][0] == 'T':
-			event = "trans"
-		elif reconciliation[key][0] == 'S':
-			event = "spec"
-		elif reconciliation[key][0] == 'L':
-			event = "loss"
-		else:
-			event = "gene"
-		f.write(key[0] + '\t' + key[1] + '\t' + event + '\n')
+		event = reconciliation[key][0]
+		f.write(key[0] + '\t' + key[1] + '\t' + D[event] + '\n')
 	f.close()
 
