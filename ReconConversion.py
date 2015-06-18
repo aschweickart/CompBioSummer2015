e878bd70d4e4372cf278e7d62f8501ba99ad3daa
Carter <carter440@gmail.com>
2015-06-18 00:09:16 -0700

    fixes

--- ReconConversion.py
+++ ReconConversion.py
@@ -3,8 +3,8 @@ def convert(reconciliation, outputFile):
 	"""Takes as input a dictionary of a reconciliation between host and parasite trees and a string containing the 
 	name of a file where it will put the output. The function outputs the same tree converted to brecon format."""
 	
-	D = {'T': 'trans', 'S': 'spec', 'L': 'loss', 'C': 'gene'}
-	f = open(outputFile + ".mowgli.brecon", 'w')
+	D = {'T': 'trans', 'S': 'spec', 'L': 'loss', 'C': 'gene', 'D': 'dup'}
+	f = open( "tree and smap files/" + outputFile + ".mowgli.brecon", 'w')
 	event = ""
 	for key in reconciliation:
 		event = reconciliation[key][0]
--- newickToVis.py
+++ newickToVis.py
@@ -5,16 +5,9 @@ def convert(fileName):
     P,H,phi = contents.split(";")
     P = P.strip()
     H = H.strip()
-    phi = phi.strip()
-    print phi
-    f = open('tree and smap files/' + fileName[:-7] + '.smap', 'w')
-    for item in phi:
-        item = item.replace(":"," ")
-        f.write(item)
+    f = open('tree and smap files/' + fileName[:-7] + '.stree', 'w')
+    f.write(P + ";")
     f.close()
     f = open('tree and smap files/' + fileName[:-7] + '.tree', 'w')
-    f.write(P)
-    f.close()
-    f = open('tree and smap files/' + fileName[:-7] + '.stree', 'w')
-    f.write(H)
+    f.write(H + ";")
     f.close
