53a1a89818b3d2e222cbea03eb8c77c7d0e04d8d
Carter <carter440@gmail.com>
2015-06-18 02:09:21 -0700

    Fixin Stuffs

--- orderGraph.py
+++ orderGraph.py
@@ -1,28 +1,28 @@
 import copy
 
-def findLoner(reco):
-	for key in reco:
-		if reco[key] == 0:
-			return key
-	return None
-
 def date(recon):
 	order = {}
-	recopy = copy.deepcopy(recon)
 	dicto = {}
+	LonerList = []
 	for key in recopy:
 		dicto[key] = 0
 	for key in recopy:
 		for child in recopy[key]:
 			dicto[child] += 1
 	place = 0
-	while recopy:
-		x = findLoner(recopy)
-		if x == None:
-			return None
+	for key in recon:
+		if dicto[key] == 0:
+			LonerList += key
+	while recopy and LonerList:
+		x = LonerList[0]
 		order[x] = place
 		place += 1
 		for child in recopy[x]:
 			dicto[child] = dicto[child] - 1
+			if dicto[child] == 0:
+				LonerList += child
+		del LonerList[0]
 		del recopy[x]
+	if recopy:
+		return None
 	return order
