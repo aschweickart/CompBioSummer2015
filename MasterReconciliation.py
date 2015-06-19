22f0003427bd04e70738ed6e326ecb63b5f5d939
Carter <carter440@gmail.com>
2015-06-18 20:13:33 -0700

    changing order to work better with vistrans

--- MasterReconciliation.py
+++ MasterReconciliation.py
@@ -5,9 +5,9 @@ import ReconConversion
 import orderGraph
 
 def Reconcile(fileName, D, T, L, k):
-	newickToVis.convert(fileName)
 	host, paras, phi = DP.newickFormatReader.getInput(fileName)
 	DTL = DP.DP(host, paras, phi, D, T, L)
 	rec = Greedy.Greedy(DTL, paras, k)
 	ReconConversion.convert(rec[0], fileName[:-7])
+	newickToVis.convert(fileName)
 	return DTL, rec
--- newickToVis.py
+++ newickToVis.py
@@ -1,13 +1,13 @@
-def convert(fileName):
+
+import orderGraph
+
+def convert(fileName, HostOrder):
     f = open(fileName, 'r')
     contents = f.read()
     f.close()
     H,P,phi = contents.split(";")
     P = P.strip()
     H = H.strip()
-    H = H.replace(',', ':1,')
-    H = H.replace(')', ':1)')
-    H = H + ":1;"
     f = open('tree and smap files/' + fileName[:-7] + '.stree', 'w')
     f.write(H)
     f.close()
