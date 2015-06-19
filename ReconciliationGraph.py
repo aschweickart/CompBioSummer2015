d0a12b2f9d2f07a8248d8099c5605d13042efba4
Juliet <jforman@hmc.edu>
2015-06-19 10:54:09 -0700

    fix treeFormat

--- ReconciliationGraph.py
+++ ReconciliationGraph.py
@@ -1,58 +1,71 @@
-H = {('h6', 'h8'): ('h6', 'h8', ('h8', 'h3'), ('h8', 'h4')), ('h8', 'h3'):
-('h8', 'h3', None, None), ('h6', 'h7'): ('h6', 'h7', ('h7', 'h1'), ('h7', 'h2')), 'hTop':
-('Top', 'h6', ('h6', 'h7'), ('h6', 'h8')), ('h7', 'h2'): ('h7', 'h2', None, None), ('h8', 'h4'):
-('h8', 'h4', None, None), ('h7', 'h1'): ('h7', 'h1', None, None)}
+H = {('h6', 'h8'): ('h6', 'h8', ('h8', 'h3'), ('h8', 'h4')), 
+('h8', 'h3'): ('h8', 'h3', None, None), 
+('h6', 'h7'): ('h6', 'h7', ('h7', 'h1'), ('h7', 'h2')), 
+'hTop': ('Top', 'h6', ('h6', 'h7'), ('h6', 'h8')), 
+('h7', 'h2'): ('h7', 'h2', None, None), 
+('h8', 'h4'): ('h8', 'h4', None, None), 
+('h7', 'h1'): ('h7', 'h1', None, None)}
 
 
-P = {('p8', 'p3'): ('p8', 'p3', None, None), 
-('p6', 'p8'): ('p6', 'p8', ('p8', 'p2'), ('p8', 'p3')), 
-'pTop': ('Top', 'p6', ('p6', 'p1'), ('p6', 'p8')), 
-('p6', 'p1'): ('p6', 'p1', None, None), 
-('p8', 'p2'): ('p8', 'p2', None, None)}
+P = {('p6', 'p8'): ('p6', 'p8', ('p8', 'p3'), ('p8', 'p4')), 
+('p7', 'p2'): ('p7', 'p2', None, None),
+('p6', 'p7'): ('p6', 'p7', ('p7', 'p1'), ('p7', 'p2')), 
+('p8', 'p4'): ('p8', 'p4', None, None),
+('p8', 'p3'): ('p8', 'p3', None, None), 
+'pTop': ('Top', 'p6', ('p6', 'p7'), ('p6', 'p8')), 
+('p7', 'p1'): ('p7', 'p1', None, None)}
 
-greedy = {('p8', 'h2'): ['T', ('p7', 'h2'), ('p5', 'h5')], 
-('p4', 'h3'): ['C', (None, None), (None, None)], 
-('p5', 'h5'): ['C', (None, None), (None, None)], 
-('p2', 'h4'): ['C', (None, None), (None, None)], 
+R = {('p8', 'h2'): ['T', ('p3', 'h2'), ('p4', 'h4')], 
+('p7', 'h1'): ['T', ('p1', 'h1'), ('p2', 'h3')], 
 ('p1', 'h1'): ['C', (None, None), (None, None)], 
+('p6', 'h7'): ['S', ('p7', 'h1'), ('p8', 'h2')], 
 ('p3', 'h2'): ['C', (None, None), (None, None)], 
-('p6', 'h1'): ['T', ('p1', 'h1'), ('p2', 'h4')], 
-('p9', 'h6'): ['S', ('p6', 'h1'), ('p8', 'h2')], 
-('p7', 'h2'): ['T', ('p3', 'h2'), ('p4', 'h3')]}
+('p2', 'h3'): ['C', (None, None), (None, None)], 
+('p4', 'h4'): ['C', (None, None), (None, None)]}
+
+def findRoot(Tree):
+    """This function takes in a parasiteTree and returns a string with the name of
+    the root vertex of the tree"""
+
+    if 'pTop' in Tree:
+    	return Tree['pTop'][1]
+    return Tree['hTop'][1]
 
 def InitDicts(tree):
 	"""This function takes as input a tree dictionary and returns a dictionary with all of the bottom nodes 
 	of the edges as keys and empty lists as values."""
 	treeDict = {}
-	parentDict = {}
 	for key in tree:
-		if key == 'pTop' or key == 'hTop':
-			treeDict[key] = []
-			parentDict[key] = tree[key][0]
+		if key == 'pTop':
+			treeDict[P[key][1]] = [] 
+		elif key == 'hTop':
+			treeDict[H[key][1]] = []
 		else:
 			treeDict[key[1]] = []
-			parentDict[key[1]] = tree[key][0]
-	return treeDict, parentDict
+	return treeDict
 
 def treeFormat(tree):
 	"""Takes a tree in the format that it comes out of newickFormatReader and converts it into a dictionary
 	with keys which are the bottom nodes of the edge and values which are the children."""
-
-	treeDict, parentDict = InitDicts(tree)
+	treeDict = InitDicts(tree)
+	treeRoot = findRoot(tree)
 	for key in tree:
-		if key == 'pTop' or key == 'hTop':
+		if key == 'hTop' or key == 'pTop':
 			if tree[key][-2] == None:
-				treeDict[key] = treeDict[key] + [tree[key][-2]]
+				treeDict[treeRoot] = treeDict[treeRoot] + [tree[key][-2]]
 			else:
-				treeDict[key] = treeDict[key] + [tree[key][-2][1]]
+				treeDict[treeRoot] = treeDict[treeRoot] + [tree[key][-2][1]]
 			if tree[key][-1] == None:
-				treeDict[key] = treeDict[key] + [tree[key][-1]]
+				treeDict[treeRoot] = treeDict[treeRoot] + [tree[key][-1]]
 			else:
-				treeDict[key] = treeDict[key] + [tree[key][-1][1]]
+				treeDict[treeRoot] = treeDict[treeRoot] + [tree[key][-1][1]]
 		else:
 			if tree[key][-2] == None:
 				treeDict[key[1]] = treeDict[key[1]] + [tree[key][-2]]
 			else:
+				print "key:", key
+				print "key[1]:", key[1]
+				print "tree[key][-2][1]:", tree[key][-2][1]
 				treeDict[key[1]] = treeDict[key[1]] + [tree[key][-2][1]]
 			if tree[key][-1] == None:
 				treeDict[key[1]] = treeDict[key[1]] + [tree[key][-1]]
@@ -67,17 +80,17 @@ def parentsDict(H, P):
 	parentsDict = {}
 	for key in H:
 		if key == 'hTop':
-			parentsDict[key] = H[key][0]
+			parentsDict[H[key][1]] = H[key][0]
 		else:
 			parentsDict[key[1]] = H[key][0]
 	for key in P:
 		if key == 'pTop':
-			parentsDict[key] = P[key][0]
+			parentsDict[P[key][1]] = P[key][0]
 		else:
 			parentsDict[key[1]] = P[key][0]
 	return parentsDict
 
-def buildReconstruction(HostTree, ParasiteTree, reconciliation):
+def build(HostTree, ParasiteTree, reconciliation):
 	""" """
 
 	parents = parentsDict(HostTree, ParasiteTree)
@@ -94,8 +107,12 @@ def buildReconstruction(HostTree, ParasiteTree, reconciliation):
 			reconGraph[parent2] = reconGraph[parent2] + [reconciliation[key][2][1]]
 
 		elif reconciliation[key][0] == 'S':
-			parent = parents(key[0])
-			reconGraph[parent] = reconGraph[parent] + [key[1]]
+			parent = parents[key[0]]
+			if parent != 'Top':
+				reconGraph[parent] = reconGraph[parent] + [key[1]]
+			print "key[1]:", key[1]
+			print "key[0]:", key[0]
+			print reconGraph
 			reconGraph[key[1]] = reconGraph[key[1]] + reconGraph[key[0]]
 			del reconGraph[key[0]]
 
@@ -107,6 +124,7 @@ def buildReconstruction(HostTree, ParasiteTree, reconciliation):
 		elif reconciliation[key][0] == 'C':
 			reconGraph[key[1]] = ['tip']
 	return reconGraph
+
 
 
 
