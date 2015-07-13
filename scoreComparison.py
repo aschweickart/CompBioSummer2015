a9141474df983c09a4f209754135be0c20261f12
Srinidhi Srinivasan <srsrinivasan@hmc.edu>
2015-07-13 16:51:21 -0700

    comment using programming conventions

--- scoreComparison.py
+++ scoreComparison.py
@@ -1,11 +1,16 @@
-#scoreComparison.py
+# scoreComparison.py
+# Srinidhi Srinivasan
+# July 2015
 
-#carter: getting input three lists where each element in the list is in
-#in the form of a tuple: (total # events, # events unique)
+# This file contains functions for finding the percent difference between the
+# scoring functions. The main functions in this file are the two wrapper 
+# functions: averageDifferentEvents, and sumDifferentEvents, and the rest of
+# the functions are helper function that are used by the wrapper functions.
 
 
 def averageDifferentEvents():
-	""" """
+	"""This function is a wrapper function that calculates the average percent
+	difference between the scoring functions."""
 	frequencyL, unitL, costscapeL = diffEvents() #input carter's function name
 	percentFreqDict = makePercentDict(frequencyL)
 	percentUnitDict = makePercentDict(unitL)
@@ -19,7 +24,7 @@ def makePercentDict(scoreL):
 	"""This function takes in a scoringList where each element in the list is 
 	in the form of a tuple: (total # events, # events unique) and returns a 
 	dictionary where the keys are tuples and the values are the decimal value 
-	difference betweent the unique events and all the events."""
+	difference between the unique events and all the events."""
 	totalEvents = 0
 	uniqueEvents = 0
 	scoringDict = {}
@@ -30,18 +35,22 @@ def makePercentDict(scoreL):
 	return scoringDict
 
 def calcAvgPercent(scoreDict):
-	""" """
+	"""This function takes as input a scoreDict, a dictionary whose keys are
+	tuples and whose values are the decimal value difference between the 
+	unique events and all the events and returns the average percent
+	for a particular comparison."""
 	avgPercent = 0
 	totalPercent = 0
 	numElements = len(scoreDict)
-	for key in percentFreqDict:
+	for key in scoreDict:
 		totalPercent += scoreDict[key]
 
 	avgPercent = totalPercent/numElements*100
 	return avgPercent
 
 def sumDifferentEvents():
-	""" """
+	"""This function is a wrapper function that calculates the sum percent
+	difference between the scoring functions."""
 	frequencyL, unitL, costscapeL = diffEvents() #input carter's function name
 	sumFreqPercent = calcSumPercent(frequencyL)
 	sumUnitPercent = calcSumPercent(unitL)
@@ -49,7 +58,10 @@ def sumDifferentEvents():
 	return sumFreqPercent, sumUnitPercent, sumCostPercent
 
 def calcSumPercent(scoreL):
-	""" """
+	"""This function takes as input scoreL, which is a list where each element
+	is a tuple that include the total number of events as well as the number 
+	of unique events, and returns the sumPercent for that particular
+	scoring function comparison."""
 	totalEvents = 0
 	uniqueEvents = 0
 	sumPercent = 0
