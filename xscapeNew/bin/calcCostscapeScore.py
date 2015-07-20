d0cfa642ecffec9f6f4c1cada07b6eeca03ab371
Juliet <jforman@hmc.edu>
2015-07-20 15:28:11 -0700

    line length

--- xscapeNew/bin/calcCostscapeScore.py
+++ xscapeNew/bin/calcCostscapeScore.py
@@ -2,11 +2,11 @@
 # Juliet Forman and Srinidhi Srinivasan
 # July 2015
 
-# This file contains functions for calculating scores for events in the DTLReconGraph 
-# based on their frequency among the different regions of costscape. The 
-# score for each event is the fraction of regions in costscape in which the 
-# event appears. The main function is newScoreWrapper, and the other 
-# functions are helper functions for newScoreWrapper.
+# This file contains functions for calculating scores for events in the 
+# DTLReconGraph based on their frequency among the different regions of 
+# costscape. The score for each event is the fraction of regions in costscape 
+# in which the event appears. The main function is newScoreWrapper, and the 
+# other functions are helper functions for newScoreWrapper.
 
 from DP import *
 from costscapeScore import *
@@ -15,8 +15,8 @@ import newickFormatReader
 
 
 def deleteCommas(pointList):
-	"""This function takes as input a list of points, returns a that same list 
-	with all commas removed."""
+	"""This function takes as input a list of points, returns a that same 
+	list with all commas removed."""
 
 	for point in pointList:
 		point.strip(',')
@@ -40,24 +40,28 @@ def getDTLReconGraphVals(pointList):
 	return DTLReconGraphPairs
 
 
-def getCostscapeDTLReconGraphs(DTLReconGraphPairs, hostTree, parasiteTree, phi):
-	"""This function takes as input DTLReconGraphPairs, a list of tuples with transfer and loss 
-	costs, and the hostTree, parasiteTree, and phi. It returns a list of DTLReconGraphs 
-	whose scores are computed with the transfer and loss values from each element in 
-	DTLReconGraphPairs."""
+def getCostscapeDTLReconGraphs(DTLReconGraphPairs, hostTree, parasiteTree, \
+		phi):
+	"""This function takes as input DTLReconGraphPairs, a list of tuples with 
+	transfer and loss costs, and the hostTree, parasiteTree, and phi. It 
+	returns a list of DTLReconGraphs whose scores are computed with the 
+	transfer and loss values from each element in DTLReconGraphPairs."""
 	
 	DTLReconGraphList = []
 	for cost in DTLReconGraphPairs:
 		#assign those associated costs to the newDTLReconGraph
-		newDTLReconGraph = DP(hostTree, parasiteTree, phi, 1, cost[0], cost[1])[0]
+		newDTLReconGraph = DP(hostTree, parasiteTree, phi, 1, cost[0], \
+			cost[1])[0]
 		DTLReconGraphList.append(newDTLReconGraph)
 	return DTLReconGraphList
 
 
 def changeDTLReconGraphScores(originalDTLReconGraph, DTLReconGraphList):
-	"""This function takes as input the originalDTLReconGraph and a list DTLReconGraphList of the 
-	DTLReconGraphs from each region in costscape. This function calculates a new score 
-	for each event in originalDTLReconGraph, and returns a newDTLReconGraph with these scores."""
+	"""This function takes as input the originalDTLReconGraph and a list 
+	DTLReconGraphList of the DTLReconGraphs from each region in costscape. 
+	This function calculates a new score for each event in 
+	originalDTLReconGraph, and returns a newDTLReconGraph with these 
+	scores."""
 
 	newDTLReconGraph = {}
 	numDTLReconGraph = len(DTLReconGraphList)
@@ -75,15 +79,17 @@ def changeDTLReconGraphScores(originalDTLReconGraph, DTLReconGraphList):
 
 def newScoreWrapper(newickFile, switchLo, switchHi, lossLo, lossHi, D, T, L):
 	"""This function takes as input hostTree, parasiteTree, phi, duplication 
-	cost D, transfer cost T, and loss cost L, and returns the newDTLReconGraph whose 
-	scores were calculated from costscape."""
+	cost D, transfer cost T, and loss cost L, and returns the 
+	newDTLReconGraph whose scores were calculated from costscape."""
 
 	H, P, phi = newickFormatReader.getInput(newickFile)
 	originalDTLReconGraph, numRecon, leaves = DP(H, P, phi, D, T, L)
 	pointList = findCenters(newickFile, switchLo, switchHi, lossLo, lossHi)
 	DTLReconGraphPairs = getDTLReconGraphVals(pointList)
-	DTLReconGraphList = getCostscapeDTLReconGraphs(DTLReconGraphPairs, H, P, phi)
-	newDTLReconGraph = changeDTLReconGraphScores(originalDTLReconGraph, DTLReconGraphList)
+	DTLReconGraphList = getCostscapeDTLReconGraphs(DTLReconGraphPairs, H, P, \
+		phi)
+	newDTLReconGraph = changeDTLReconGraphScores(originalDTLReconGraph, \
+		DTLReconGraphList)
 	return newDTLReconGraph, numRecon, leaves
 
 
