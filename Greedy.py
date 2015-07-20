fecadf89e4cc497e757878769887d833bed6bbf9
Juliet <jforman@hmc.edu>
2015-07-20 15:47:33 -0700

    line length

--- Greedy.py
+++ Greedy.py
@@ -3,25 +3,26 @@
 # June 2015
 
 # This file contains the functions for finding all optimal reconciliations 
-# using a DTL reconciliation graph, as well as the scores for each of the events using the
-# vertex-based DP algorithm. The main function in this file is called Greedy
-# and the remaining functions are helper functions that are used by Greedy.
+# using a DTL reconciliation graph, as well as the scores for each of the 
+# events using thevertex-based DP algorithm. The main function in this file 
+# is called Greedy and the remaining functions are helper functions that are 
+# used by Greedy.
 
 
 def findRoot(tree):
-    """This function takes in a parasite tree or a host treeand returns a string with the 
-    name of the root vertex of the tree"""
+    """This function takes in a parasite tree or a host treeand returns a 
+    string with the name of the root vertex of the tree"""
 
     if 'pTop' in tree:
         return tree['pTop'][1]
     return tree['hTop'][1] 
 
 def orderDTLReconGraph(DTLReconGraph, parasiteRoot):
-    """This function takes in a DTL reconciliation graph and the parasiteRoot. It outputs a 
-    list, keysL, that contains tuples. Each tuple has two elements. The first
-    is a mapping node of the form (p, h), where p is a parasite node and h is 
-    a host node. The second element is a level representing the depth of that
-    mapping node within the tree."""
+    """This function takes in a DTL reconciliation graph and the 
+    parasiteRoot. It outputs a list, keysL, that contains tuples. Each tuple 
+    has two elements. The firstis a mapping node of the form (p, h), where p 
+    is a parasite node and h is a host node. The second element is a level 
+    representing the depth of thatmapping node within the tree."""
 
     keysL = []
     topNodes = []
@@ -33,12 +34,13 @@ def orderDTLReconGraph(DTLReconGraph, parasiteRoot):
     return keysL
 
 def orderDTLReconGraphRoots(DTLReconGraph, vertex, level):
-    """This function takes a DTL reconciliation graph, one node, a vertex, of the DTL reconciliation graph, 
-    and level, and returns a list, keysL, that contains tuples. Each tuple has
-    two elements. The first is a mapping node of the form (p, h), where p is a
-    parasite node and h is a host node. The second element is a level 
-    representing the depth of that mapping node within the tree. This function
-    adds the input vertex to keysL and recurses on its children."""
+    """This function takes a DTL reconciliation graph, one node, a vertex, of 
+    the DTL reconciliation graph, and level, and returns a list, keysL, that 
+    contains tuples. Each tuple has two elements. The first is a mapping node 
+    of the form (p, h), where p is a parasite node and h is a host node. The 
+    second element is a level representing the depth of that mapping node 
+    within the tree. This function adds the input vertex to keysL and 
+    recurses on its children."""
 
     keysL = []
     #loop through each event associated with key in DTLReconGraph
@@ -46,15 +48,19 @@ def orderDTLReconGraphRoots(DTLReconGraph, vertex, level):
         _, child1, child2, _ = event
         keysL = keysL + [(vertex, level)]
         if child1[0] != None:
-            keysL.extend(orderDTLReconGraphRoots(DTLReconGraph, child1, level + 1))
+            keysL.extend(orderDTLReconGraphRoots(DTLReconGraph, child1, \
+                level + 1))
         if child2[0] != None:
-            keysL.extend(orderDTLReconGraphRoots(DTLReconGraph, child2, level + 1)) 
+            keysL.extend(orderDTLReconGraphRoots(DTLReconGraph, child2, \
+                level + 1)) 
     return keysL
 
 
 def sortHelper(DTLReconGraph, keysL):
-    """This function takes in a list keysL, where the keys are mapping nodes and the values are the level that represents the depth of that mapping node within the tree, and a DTL reconciliation graph, and returns a new 
-    list, uniqueKeysL that has removed duplicate mapping nodes from keysL.
+    """This function takes in a list keysL, where the keys are mapping nodes 
+    and the values are the level that represents the depth of that mapping 
+    node within the tree, and a DTL reconciliation graph, and returns a new 
+    list, uniqueKeysL that has removed duplicate mapping nodes from keysL. 
     This function chooses the highest level for each mapping node because we 
     are using the bottom-up approach."""
     
@@ -70,9 +76,10 @@ def sortHelper(DTLReconGraph, keysL):
 
 
 def postorderDTLReconGraphSort(DTLReconGraph, parasiteRoot):
-    """This function takes in a DTL reconciliation graph and parasiteRoot, and returns a 
-    sorted list, orderedKeysL, that is ordered by level from largest to 
-    smallest, where level 0 is the root and the highest level are tips."""
+    """This function takes in a DTL reconciliation graph and parasiteRoot, 
+    and returns a sorted list, orderedKeysL, that is ordered by level from 
+    largest to smallest, where level 0 is the root and the highest level are 
+    tips."""
 
     keysL = orderDTLReconGraph(DTLReconGraph, parasiteRoot)
     uniqueKeysL = sortHelper(DTLReconGraph, keysL)
@@ -87,13 +94,14 @@ def postorderDTLReconGraphSort(DTLReconGraph, parasiteRoot):
 
 
 def BSFHAlgorithm(DTLReconGraph, parasiteTree):
-    """This function takes as inputs a DTL reconciliation graph and parasiteTree, and then 
-    records what the max is at each mapping node and which event the max came 
-    from. It outputs a dictionary BSFHMap by looping through the keys in a 
-    sorted list of mapping nodes and finding the max score at each mapping 
-    node and event node. BSFHMap has keys of the form (p, h) which are the 
-    mapping nodes, and values which are lists where the first element is an 
-    event with the max score, and the last element is the maxScore. Note that BSFH stands for Best Score From Here"""
+    """This function takes as inputs a DTL reconciliation graph and 
+    parasiteTree, and then records what the max is at each mapping node and 
+    which event the max came from. It outputs a dictionary BSFHMap by looping 
+    through the keys in a sorted list of mapping nodes and finding the max 
+    score at each mapping node and event node. BSFHMap has keys of the form 
+    (p, h) which are the mapping nodes, and values which are lists where the 
+    first element is an event with the max score, and the last element is the 
+    maxScore. Note that BSFH stands for Best Score From Here"""
 
     #Example: BSFHMap = {(mapping node): [['event', (p, h), (p, h), score], 
     #                                                               maxScore]}
@@ -107,7 +115,8 @@ def BSFHAlgorithm(DTLReconGraph, parasiteTree):
         mapNode = key[0]
         #check if the key is a tip:
         if DTLReconGraph[mapNode][0][0] == 'C':              
-            BSFHMap[mapNode] = [DTLReconGraph[mapNode][0], DTLReconGraph[mapNode][0][-1]]
+            BSFHMap[mapNode] = [DTLReconGraph[mapNode][0], \
+                DTLReconGraph[mapNode][0][-1]]
         #if the key isn't a tip:
         else: 
             #initialize counter                                    
@@ -131,16 +140,19 @@ def BSFHAlgorithm(DTLReconGraph, parasiteTree):
 
 
 def traceChildren(DTLReconGraph, GreedyOnce, BSFHMap, key):
-    """This function takes as input a DTL reconciliation graph, a dictionary GreedyOnce, 
-    containing the root of an optimal reconciliation, a BSFHMap dicitonary, 
-    and a current key, and returns the optimal reconciliation and new DTLReconGraph 
-    graph, where the scores of the events in the reconciliation are reset to
-    0."""
+    """This function takes as input a DTL reconciliation graph, a dictionary 
+    GreedyOnce, containing the root of an optimal reconciliation, a BSFHMap 
+    dicitonary, and a current key, and returns the optimal reconciliation and 
+    new DTLReconGraph graph, where the scores of the events in the 
+    reconciliation are reset to 0."""
 
     resetDTLReconGraph = {} #the new DTLReconGraph graph
-    reset1DTLReconGraph = {} #this DTLReconGraph graph deals with the recursive call on the child1
-    reset2DTLReconGraph = {} #this DTLReconGraph graph deals with the recursive call on the child2
+    # initiate a DTLReconGraph to deal with the recursive call on child1
+    reset1DTLReconGraph = {}
+    # initiate a DTLReconGraph to deal with the recursive call on child2
+    reset2DTLReconGraph = {}
     _, child1, child2 = GreedyOnce[key]
+
     if child1 != (None, None):
         GreedyOnce[child1] = BSFHMap[child1][0][0:3] #add event to greedyOnce
         #this loop resets all the scores of events that have been used to 0 
@@ -149,10 +161,13 @@ def traceChildren(DTLReconGraph, GreedyOnce, BSFHMap, key):
                 newValue = DTLReconGraph[child1]
                 newValue[index][-1] = 0
                 reset1DTLReconGraph[child1] = newValue
-        #this recursive call updates GreedyOnce and the DTL reconciliation graph
-        newGreedyOnce, DTLReconGraph1 = traceChildren(DTLReconGraph, GreedyOnce, BSFHMap, child1) 
-        reset1DTLReconGraph.update(DTLReconGraph1) #update the dictionary to contain info about child1
+        #this recursive call updates GreedyOnce and the DTL recon graph
+        newGreedyOnce, DTLReconGraph1 = traceChildren(DTLReconGraph, \
+            GreedyOnce, BSFHMap, child1)
+        #update the dictionary to contain info about child1
+        reset1DTLReconGraph.update(DTLReconGraph1) 
         GreedyOnce.update(newGreedyOnce) # update the reconciliation
+
     if child2 != (None, None):
         GreedyOnce[child2] = BSFHMap[child2][0][0:3] #add event to greedyOnce
         #this loop resets all the scores of events that have been used to 0
@@ -161,22 +176,26 @@ def traceChildren(DTLReconGraph, GreedyOnce, BSFHMap, key):
                 newValue = DTLReconGraph[child2]
                 newValue[index][-1] = 0
                 reset2DTLReconGraph[child2] = newValue    
-        # this recursive call updates GreedyOnce and the DTL reconciliation graph  
-        newGreedyOnce, DTLReconGraph2 = traceChildren(DTLReconGraph, GreedyOnce, BSFHMap, child2)
-        reset2DTLReconGraph.update(DTLReconGraph2) # update dictionary to contain info about child2
+        # this recursive call updates GreedyOnce and the DTL recon graph  
+        newGreedyOnce, DTLReconGraph2 = traceChildren(DTLReconGraph, \
+            GreedyOnce, BSFHMap, child2)
+        # update dictionary to contain info about child2
+        reset2DTLReconGraph.update(DTLReconGraph2)
         GreedyOnce.update(newGreedyOnce) #update the reconciliation
-    resetDTLReconGraph.update(reset1DTLReconGraph) # update the new DTL reconciliation graph
-    resetDTLReconGraph.update(reset2DTLReconGraph) # update the new DTL reconciliation graph
+
+    # update the new DTL reconciliation graph
+    resetDTLReconGraph.update(reset1DTLReconGraph)
+    resetDTLReconGraph.update(reset2DTLReconGraph)
     return GreedyOnce, resetDTLReconGraph
 
 
 def greedyOnce(DTLReconGraph, parasiteTree):
-    """This function takes the DTLReconGraph graph and the parasiteTree as inputs, and 
-    calls BSFHAlgorithm to find the dictionary BSFHMap. It returns the 
-    reconciliation with the highest score in a dictionary called GreedyOnce, 
-    and also resets to 0 the scores of the mapping nodes in the optimal 
-    reconciliation. The returned dictionary will have keys which are the 
-    mapping nodes in the optimal reconciliation, and values of the form 
+    """This function takes the DTLReconGraph graph and the parasiteTree as 
+    inputs, and calls BSFHAlgorithm to find the dictionary BSFHMap. It 
+    returns the reconciliation with the highest score in a dictionary called 
+    GreedyOnce, and also resets to 0 the scores of the mapping nodes in the 
+    optimal reconciliation. The returned dictionary will have keys which are 
+    the mapping nodes in the optimal reconciliation, and values of the form 
     (event, child1, child2)."""
 
     BSFHMap = BSFHAlgorithm(DTLReconGraph, parasiteTree)
@@ -191,34 +210,41 @@ def greedyOnce(DTLReconGraph, parasiteTree):
             bestKey = key
             bestScore = BSFHMap[key][-1]
     #set value in GreedyOnce of the best key we found
-    GreedyOnce[bestKey] = BSFHMap[bestKey][0][0:3]   #index from 0 to 3 because we amputate the score from DTLReconGraph               
+    #index from 0 to 3 because we amputate the score from DTLReconGraph
+    GreedyOnce[bestKey] = BSFHMap[bestKey][0][0:3]
     
     #reset score of the mapping node we used to 0:
 
     #loop through the events associated with DTLReconGraph
     for index in range(len(DTLReconGraph[bestKey]) - 1):  
         #check if the event matches the event that gave the best score                      
-        if DTLReconGraph[bestKey][index] == BSFHMap[bestKey][0]:           
-            newEvent = DTLReconGraph[bestKey][index][:-1] + [0] #indexing updates event in DTL and puts it back in place
-            newValue = DTLReconGraph[bestKey][:index] + [newEvent] + DTLReconGraph[bestKey][index + 1:]
+        if DTLReconGraph[bestKey][index] == BSFHMap[bestKey][0]:
+            #indexing updates event in DTL and puts it back in place
+            newEvent = DTLReconGraph[bestKey][index][:-1] + [0] 
+            newValue = DTLReconGraph[bestKey][:index] + [newEvent] + \
+                DTLReconGraph[bestKey][index + 1:]
             DTLReconGraph[bestKey] = newValue      #set the score to 0
-    newGreedyOnce, resetDTLReconGraph = traceChildren(DTLReconGraph, GreedyOnce, BSFHMap, bestKey)
+
+    newGreedyOnce, resetDTLReconGraph = traceChildren(DTLReconGraph, \
+        GreedyOnce, BSFHMap, bestKey)
     GreedyOnce.update(newGreedyOnce)
     DTLReconGraph.update(resetDTLReconGraph)
     return GreedyOnce, DTLReconGraph, bestScore
 
 def Greedy(DTLReconGraph, parasiteTree):
-    """This function takes as input a DTL reconciliation graph and a parasiteTree, and 
-    returns TreeList, a list of dictionaries, each of which represent one of 
-    the optimal reconciliations. This function runs till all the scores have 
-    been collected from the DTL reconciliation graph."""
+    """This function takes as input a DTL reconciliation graph and a 
+    parasiteTree, and returns TreeList, a list of dictionaries, each of which 
+    represent one of the optimal reconciliations. This function runs till all 
+    the scores have been collected from the DTL reconciliation graph."""
+
     scores = [] #list of reconciliation scores
     currentDTLReconGraph = DTLReconGraph
     rec = [] #list of reconciliations
     collected = True
     while collected:
         #call greedyOnce if all the points have not been collected yet
-        oneTree, currentDTLReconGraph, score = greedyOnce(currentDTLReconGraph, parasiteTree)
+        oneTree, currentDTLReconGraph, score = \
+            greedyOnce(currentDTLReconGraph, parasiteTree)
         scores.append(score) 
         rec.append(oneTree)
         collected = False #set collected to False
